4653ff96f15c33e704f83c02c920376f1a879a62
web-dev-easy <61095459+web-dev-easy@users.noreply.github.com>
2020-02-18 09:17:59 +0700

    Create fbinit.js

---
+++ js/fbinit.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+ apiKey: "AIzaSyAmOPbrm_xL3WyNN9vr5x1uSGDtMBkLL3k",
+ authDomain: "wde-fb.firebaseapp.com",
+ databaseURL: "https://wde-fb.firebaseio.com",
+ projectId: "wde-fb",
+ storageBucket: "wde-fb.appspot.com",
+ messagingSenderId: "54238040766",
+ appId: "1:54238040766:web:e34a4d61161d86518d84ef",
+ measurementId: "G-6XXRYV5L3L"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
